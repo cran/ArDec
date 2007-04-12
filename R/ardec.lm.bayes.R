4facc663626e0f704c2dd0b679672bda3ce7f8e4
S. M. Barbosa <susana.barbosa@fc.up.pt>
2007-04-12 00:00:00 +0000

    version 1.0-5

--- R/ardec.lm.bayes.R
+++ R/ardec.lm.bayes.R
@@ -1,19 +1,27 @@
-"ardec.lm.bayes" <-
-function (x, R, med = TRUE) 
-{
-    require(ardec.sampling)
-    fit = ardec.lm(x)
-    p = length(fit$coefficients)
-    phi = matrix(nrow = p, ncol = R)
-    i = 1
-    set.seed(1)
-    while (i <= R) {
-        phi[, i] = as.vector(ardec.sampling(x, fit)$ARcoef)
-        i = i + 1
-    }
-    beta = apply(phi, 1, median)
-    if (med == TRUE) {
-        return(list(BayesCoef = beta))
-    }
-    return(list(BayesCoef = phi))
+`ardec.lm.bayes` <-
+function(x, R, med=TRUE){
+
+#require(ardec.sampling)
+
+fit=ardec.lm(x)
+
+p=length(fit$coefficients)
+phi=matrix(nrow=p,ncol=R)
+
+
+i=1
+
+set.seed(1)
+
+while(i<=R){
+           phi[,i]=as.vector(ardec.sampling(x,fit)$ARcoef)  
+           i=i+1
+           }
+
+beta=apply(phi,1,median)
+
+if(med==TRUE){return(list(BayesCoef=beta))}
+
+return(list(BayesCoef=phi)) 
 }
+
--- R/ardec.trend.bayes.R
+++ R/ardec.trend.bayes.R
@@ -1,28 +1,37 @@
-"ardec.trend.bayes" <-
-function (x, R) 
-{
-    options(warn = -1)
-    l = seq(1, R, by = 1)
-    m = seq(1, R, by = 1)
-    gt = matrix(nrow = R, ncol = length(x))
-    fit = ardec.lm(x)
-    i = 1
-    set.seed(1)
-    while (i <= R) {
-        phi = as.vector(ardec.sampling(x, fit)$ARcoef)
-        comp = ardec(x, phi)
-        if (any(comp$period == Inf)) {
-            warning("no trend component")
-        }
-        if (any(comp$period == Inf)) {
-            l[i] = comp$period[which(match(comp$period, Inf) == 
-                1)[1]]
-            m[i] = comp$modulus[which(match(comp$period, Inf) == 
-                1)[1]]
-            gt[i, ] = Re(comp$comps[which(match(comp$period, 
-                Inf) == 1)[1], ])
-            i = i + 1
-        }
-    }
-    return(list(modulus = m, trendSim = gt))
+`ardec.trend.bayes` <-
+function(x, R){
+
+options(warn=-1)
+
+l=seq(1,R,by=1)
+m=seq(1,R,by=1)
+gt=matrix(nrow=R,ncol=length(x))
+
+
+fit=ardec.lm(x)
+
+i=1
+
+set.seed(1)
+
+while(i<=R){
+         phi=as.vector(ardec.sampling(x,fit)$ARcoef)
+         comp=ardec(x,phi)
+
+         
+        if (any(comp$period==Inf)){warning("no trend component")}
+
+         if(any(comp$period ==Inf)){
+         l[i]=comp$period[which(match(comp$period,Inf)==1)[1]]
+         m[i]=comp$modulus[which(match(comp$period,Inf)==1)[1]]
+         gt[i,]=Re(comp$comps[which(match(comp$period,Inf)==1 )[1],])
+         i=i+1} 
+
 }
+
+
+return(list(modulus=m,trendSim=gt))  
+
+
+}
+
