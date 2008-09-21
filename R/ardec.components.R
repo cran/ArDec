3abec738e681c94924a8e7cfc0e4d558ed921edf
S. M. Barbosa <susana.barbosa@fc.up.pt>
2008-09-21 00:00:00 +0000

    version 1.2-0

--- R/ardec.components.R
+++ R/ardec.components.R
@@ -1,12 +1,18 @@
 `ardec.components` <-
-function(object, ...) {
-
-setClass("ardec",representation(start="numeric",frequency="numeric",period="numeric",modulus="numeric",comps="matrix"),package="ArDec")
-
-periodcomps=list(periods=abs(object@period[which(which(object@modulus>=0.95)%%2==0)]),comps=ts(t(2*Re(object@comps[which(which(object@modulus>=0.95)%%2==0),])),start=object@start,frequency=object@frequency) )
-
-trendcomps=ts(Re(object@comps[which(object@period==Inf)[1],]),start=object@start,frequency=object@frequency) 
- 
- return(list(periodcomps=periodcomps,trendcomp=trendcomps))
- }
-
+function (object, th = 0.95, ...) 
+{
+    setClass("ardec", representation(start = "numeric", frequency = "numeric", 
+        period = "numeric", modulus = "numeric", comps = "matrix"), 
+        package = "ArDec")
+    periodcomps = list(periods = abs(object@period[which(which(object@modulus >= 
+        th)%%2 == 0)]), comps = ts(t(2 * Re(object@comps[which(which(object@modulus >= 
+        th)%%2 == 0), ])), start = object@start, frequency = object@frequency))
+    if (any(object@period == Inf)) {
+        trendcomps = ts(Re(object@comps[which(object@period == 
+            Inf)[1], ]), start = object@start, frequency = object@frequency)
+    }
+    if (all(object@period != Inf)) {
+        trendcomps = NULL
+    }
+    return(list(periodcomps = periodcomps, trendcomp = trendcomps))
+}
--- R/ardec.lm.bayes.R
+++ R/ardec.lm.bayes.R
@@ -1,27 +1,23 @@
 `ardec.lm.bayes` <-
-function(x, R, med=TRUE){
-
-#require(ardec.sampling)
-
-fit=ardec.lm(x)
-
-p=length(fit$coefficients)
-phi=matrix(nrow=p,ncol=R)
-
-
-i=1
-
-set.seed(1)
-
-while(i<=R){
-           phi[,i]=as.vector(ardec.sampling(x,fit)$ARcoef)  
-           i=i+1
-           }
-
-beta=apply(phi,1,median)
-
-if(med==TRUE){return(list(coefficients=beta))}
-
-return(list(coefficients=phi)) 
+function (x, method = "burg", na.action = na.contiguous, R, med = TRUE, 
+    seed = 1) 
+{
+    if (any(is.na(x))) {
+        warning("NA values in x - using ardec can give unreliable results!", 
+            call. = FALSE)
+    }
+    fit = ardec.lm(x, method = method, na.action = na.action)
+    p = length(fit$coefficients)
+    phi = matrix(nrow = p, ncol = R)
+    i = 1
+    set.seed(seed)
+    while (i <= R) {
+        phi[, i] = as.vector(ardec.sampling(x, fit)$ARcoef)
+        i = i + 1
+    }
+    beta = apply(phi, 1, median)
+    if (med == TRUE) {
+        return(list(coefficients = beta))
+    }
+    return(list(coefficients = phi))
 }
-
