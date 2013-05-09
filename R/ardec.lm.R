06df6d1b8f5868e23c94d271e17781e43ffa5919
Susana Barbosa <sabarbosa@fc.ul.pt>
2013-05-09 00:00:00 +0000

    version 2.0

--- R/ardec.lm.R
+++ R/ardec.lm.R
@@ -1,21 +1,18 @@
-`ardec.lm` <-
-function(x, method="burg",na.action=na.contiguous) {
+ardec.lm <-
+function(x) {
 
 require(stats)
 
-if(any(is.na(x))) { warning("NA values in x - using ardec can give unreliable results!", call. = FALSE) }
-
- dat=x-mean(x,na.rm=TRUE)
+ dat=x-mean(x)
  ndat=length(dat)
-
- p=ar(dat,method=method,na.action=na.action)[[1]] # p=order of autoregressive model from AIC (burg method)
+ 
+ p=ar(dat,method="burg")[[1]] # p=order of autoregressive model from AIC (burg method)
 
  # linear autoregressive model fit
-
- X=t(matrix(dat[rev(rep((1:p),ndat-p)+ rep((0:(ndat-p-1)),rep(p,ndat-p)))],p,ndat-p))
- y=rev(dat[(p+1):ndat])
+ 
+ X=t(matrix(dat[rev(rep((1:p),ndat-p)+ rep((0:(ndat-p-1)),rep(p,ndat-p)))],p,ndat-p))   
+ y=rev(dat[(p+1):ndat])    
  fit=lm(y~-1+X, x=TRUE)
-
+ 
  return(fit)
  }
-
